1ae5ab8bb92fbc23fcb961384742a49f8926cf22
Rishav Chapagain <gainrishavchap@gmail.com>
2024-01-18 22:23:06 +0545

    Sessions Added

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Confessions
 A simple project made for sending and recieving confessions
+
+
+
+#Made session just for testing purposes confession wala page ma
--- html/login.php
+++ html/login.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
   $email = $password = "";
 
   if (isset($_POST['login'])) {
@@ -15,6 +17,7 @@
       $row = mysqli_fetch_row($checkSql);
   
       if ($row[1] == $password) {
+        $_SESSION['email'] = $email;
         echo("<script> alert('You may now enter the site!'); window.location.href='./confessions.php'; </script>");
           die();
       } else {
