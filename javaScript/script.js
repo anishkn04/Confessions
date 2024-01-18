77799977233b73495380f44b51e527ab0843b1ef
Rishav Chapagain <gainrishavchap@gmail.com>
2024-01-18 22:39:32 +0545

    Registration Validation

--- javaScript/script.js
+++ javaScript/script.js
@@ -2,3 +2,44 @@ function loginRedirect() {
   window.location.href = "../html/login.php";
 }
 
+
+function isStrongPassword(password) {
+  if (password.length < 8) {
+      alert("Password is not strong enough. It should be at least 8 characters long.");
+      return false;
+  }
+
+  if (!/[A-Z]/.test(password)) {
+      alert("Password is not strong enough. It should contain at least one uppercase letter.");
+      return false;
+  }
+
+  if (!/[a-z]/.test(password)) {
+      alert("Password is not strong enough. It should contain at least one lowercase letter.");
+      return false;
+  }
+
+  if (!/[0-9]/.test(password)) {
+      alert("Password is not strong enough. It should contain at least one digit.");
+      return false;
+  }
+
+  if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
+      alert("Password is not strong enough. It should contain at least one special character.");
+      return false;
+  }
+
+  return true;
+}
+
+function register() {
+  let password = document.getElementById("password").value;
+
+  if (isStrongPassword(password)) {
+      alert("Registration successful!");
+  }
+  else
+  {
+    return false;
+  }
+}
