07a878a68cdf4c37163025cc2c322ceee8761698
Anish Neupane <anishnep51@gmail.com>
2024-01-18 23:21:31 +0545

    Changed the error declaration

--- html/login.php
+++ html/login.php
@@ -1,35 +1,3 @@
-<?php
-session_start();
-
-  $email = $password = "";
-
-  if (isset($_POST['login'])) {
-    
-  $email    = $_POST['email'];
-  $password = $_POST['password'];
-  include './mysqlConnection.php';
-
-  $checkQuery = "SELECT email, `password` FROM users WHERE email = '$email'";
-
-  $checkSql = mysqli_query($connection, $checkQuery);
-
-  if (mysqli_num_rows($checkSql) > 0) {
-      $row = mysqli_fetch_row($checkSql);
-  
-      if ($row[1] == $password) {
-        $_SESSION['email'] = $email;
-        echo("<script> alert('You may now enter the site!'); window.location.href='./confessions.php'; </script>");
-          die();
-      } else {
-        echo("<script> document.getElementById('invalid_pass').innerText = 'Incorrect Password!'; </script>");
-      }
-  } else {
-    echo ("<script> alert('No such user found!'); window.location.href='./login.php'; </script>");
-      die();
-  }
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -61,7 +29,37 @@ session_start();
     </form>
   </div>
 
+<?php
+session_start();
 
+$email = $password = "";
+
+if (isset($_POST['login'])) {
+
+  $email    = $_POST['email'];
+  $password = $_POST['password'];
+  include './mysqlConnection.php';
+
+  $checkQuery = "SELECT email, `password` FROM users WHERE email = '$email'";
+
+  $checkSql = mysqli_query($connection, $checkQuery);
+
+  if (mysqli_num_rows($checkSql) > 0) {
+    $row = mysqli_fetch_row($checkSql);
+
+    if ($row[1] == $password) {
+      $_SESSION['email'] = $email;
+      echo ("<script> alert('You may now enter the site!'); window.location.href='./confessions.php'; </script>");
+      die();
+    } else {
+      echo ("<script> document.getElementById('invalid_password').innerText = 'Incorrect Password!'; </script>");
+    }
+  } else {
+    echo ("<script> document.getElementById('invalid_email').innerText = 'Incorrect Email!' </script>");
+    die();
+  }
+}
+?>
 
   <script src="../javaScript/script.js"></script>
 </body>
