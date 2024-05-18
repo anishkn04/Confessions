173bbe65ae82c45ef7628061c3534a12ace7d5bf
Anish Neupane <anishnep51@gmail.com>
2024-05-18 14:45:05 +0545

    Changed = to == in index

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 $request  = $_SERVER['REQUEST_URI'];
 $username = ltrim($request, "/");
 
-if($username==null || $username = ""){
+if($username==null || $username == ""){
     header("Location: ./html/confessions.php");
 }else{
     header("Location: /html/profile.php?username=$username");
