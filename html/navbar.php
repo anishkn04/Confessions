78fec6485fe6539e107f08b2cfdd012a339eb74e
Rishav Chapagain <gainrishavchap@gmail.com>
2024-05-18 14:59:07 +0545

    Profile button fixed

--- html/navbar.php
+++ html/navbar.php
@@ -6,15 +6,20 @@
   <div id="text-container">
     <p>Confess Your Love.</p>
   </div>
+
   <div id="login-container">
-    <?php if (isset($_SESSION['email'])) {
+    <?php 
+    if (isset($_SESSION['email'])) {
       $email = $_SESSION['email'];
-      ?>
-      <!-- <a href='./profile.php'>Profile</a> -->
-      <button id='profile_btn' onclick='profileRedirect()'>Profile</button>
-      <button id='logout_btn' onclick='logout()'>Logout</button>
-    <?php } else {
-      echo ("<button id='login_btn' onclick='loginRedirect()'>Login</button>");
-    } ?>
+      
+      $currentPage = basename($_SERVER['PHP_SELF']);
+      if ($currentPage !== 'profile.php') {
+        echo "<button id='profile_btn'><a href='../html/profile.php'>Profile</a></button>";
+      }
+      echo "<button id='logout_btn' onclick='logout()'>Logout</button>";
+    } else {
+      echo "<button id='login_btn' onclick='loginRedirect()'>Login</button>";
+    } 
+    ?>
   </div>
 </nav>
