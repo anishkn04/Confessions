9b1b4371aa04925916a4a4f49870d4844bd2df20
Rishav Chapagain <gainrishavchap@gmail.com>
2024-05-18 23:05:30 +0545

    Minor fix

--- index.php
+++ index.php
@@ -3,8 +3,13 @@
 $request  = $_SERVER['REQUEST_URI'];
 $username = ltrim($request, "/");
 
-if($username==null || $username == "" || !$username="index.php"){
+// Check if $username is empty, null, or "index.php"
+if ($username == null || $username == "" || $username == "index.php") {
     header("Location: ./html/confessions.php");
-}else{
+    exit();
+} else {
+    // Ensure the username is safe for use in a URL
+    $username = htmlspecialchars($username, ENT_QUOTES, 'UTF-8');
     header("Location: /html/profile.php?username=$username");
+    exit();
 }
